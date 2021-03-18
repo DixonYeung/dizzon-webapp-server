fa8bc93ef143bcebb7487b7743e25062a2986f5a
DixonYeung <dixonycf@gmail.com>
2021-03-18 17:29:09 +0800

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -3,29 +3,38 @@ import './App.css';
 import Axios from "axios";
 
 function App() {
-  const [regemail, setregEmail] = useState('');
-  const [regpassword, setregPassword] = useState('');
-  const [regclinicName, setregClinicName] = useState('');
-  const [regphoneNumber, setregPhoneNumber] = useState('');
-  const [regaddress, setregAddress] = useState('');
-  const [userLoginEmail, setUserLoginEmail] = useState('');
-  const [userLoginPassword, setUserLoginPassword] = useState('');
 
-  const [doctor_name, setNewDoctorName] = useState('');
-  const [patient_name, setNewPatientName] = useState('');
-  const [diagnosis, setNewDiagnosis] = useState('');
-  const [medication, setNewMedication] = useState('');
-  const [consultation_fee, setNewConsultationFee] = useState('');
-  const [date_time, setNewDateTime] = useState('');
-  const [follow_up, setNewFollowUp] = useState('');
+  //new code
+  const [regInputFieldValue, setRegInputFieldValue] = useState({
+    email: '', 
+    password: '',
+    clinicName: '',
+    phoneNumber: '',
+    address: ''
+  });
+
+  const [loginFieldValue, setLoginFieldValue] = useState({
+    userLoginEmail: '',
+    userLoginPassword: ''
+  });
+
+  const [newConsultationRecord, setNewConsultationRecord] = useState({
+    doctor_name: '', 
+    patient_name: '',
+    diagnosis: '',
+    medication: '',
+    consultation_fee: '',
+    date_time: '',
+    follow_up: ''
+  });
 
   const [userList, setUserList] = useState([]);
   const [recordList, setRecordList] = useState([]);
 
   const [isLoggedIn, setIsLoggedIn] = useState();
-  // const [registerPage, setRegisterPage] = useState();
-  // const [loginPage, setLoginPage] = useState();
   const [pageState, setPageState] = useState('');
+
+
 
   const getUser = ()=>{
     Axios.get('http://localhost:4469/api/get').then(
@@ -37,7 +46,6 @@ function App() {
   const getRecord = () =>{
     Axios.get("http://localhost:4469/api/record").then((response)=>{
       if(response.data.length > 0){
-        // console.log(response);
         setRecordList(response.data);
       }
       else{
@@ -51,7 +59,6 @@ function App() {
     Axios.get("http://localhost:4469/api/login").then((response)=>{
       if(response.data.loggedIn === true){
         setUserList(response.data.user);
-        console.log(response);
         setPageState("login-browse-record");
         setIsLoggedIn(true);
       }
@@ -70,11 +77,11 @@ function App() {
 
   const registerButtonClicked = () => {
     Axios.post('http://localhost:4469/api/register',{
-      email: regemail,
-      password: regpassword,
-      clinicName: regclinicName,
-      phoneNumber: regphoneNumber,
-      address: regaddress
+      email: regInputFieldValue['email'],
+      password: regInputFieldValue['password'],
+      clinicName: regInputFieldValue['clinicName'],
+      phoneNumber: regInputFieldValue['phoneNumber'],
+      address: regInputFieldValue['address']
     }).then( () => {
       alert("sucessful inserted");
       clearField("registerFormInput");
@@ -85,8 +92,8 @@ function App() {
 
   const loginButtonClicked = () => {
     Axios.post('http://localhost:4469/api/login',{
-      email: userLoginEmail,
-      password: userLoginPassword
+      email: loginFieldValue['userLoginEmail'],
+      password: loginFieldValue['userLoginPassword']
     }).then( (response) => {
       if(response.data.message !== "" && response.data.length > 0){
         alert("sucessful login");
@@ -103,8 +110,6 @@ function App() {
 
   const logoutButtonClicked = () => {
     Axios.post('http://localhost:4469/api/logout',{
-      // email: userLoginEmail,
-      // password: userLoginPassword
     }).then( (response) => {
       if(response.data.loggedIn === false){
         setPageState("login");
@@ -112,26 +117,18 @@ function App() {
         clearField("registerFormInput");
         getUser();
       }
-      // if(response.data.message !== "" && response.data.length > 0){
-      //   alert("sucessful login");
-      //   setUserList(response.data);
-      //   setIsLoggedIn(true);
-      // }
-      // else{
-      //   alert(response.data.message);
-      // }
     });
   }
 
   const createRecordSumbitButtonClicked = () => {
     Axios.post('http://localhost:4469/api/createNewConsultationRecord',{
-      doctor_name: doctor_name,
-      patient_name: patient_name,
-      diagnosis: diagnosis,
-      medication: medication,
-      consultation_fee: consultation_fee,
-      date_time: date_time,
-      follow_up: follow_up
+      doctor_name: newConsultationRecord['doctor_name'],
+      patient_name: newConsultationRecord['patient_name'],
+      diagnosis: newConsultationRecord['diagnosis'],
+      medication: newConsultationRecord['medication'],
+      consultation_fee: newConsultationRecord['consultation_fee'],
+      date_time: newConsultationRecord['date_time'],
+      follow_up: newConsultationRecord['follow_up']
     }).then( () => {
       alert("sucessful inserted a consultation record");
       clearField('consultationRecordInput');
@@ -141,91 +138,77 @@ function App() {
   }
 
   const clearField = (mode) => {
-    if(mode === "registerFormInput"){
-      let formInputs = document.querySelectorAll(".form input[type='text']");
-      formInputs.forEach((item, index)=>{
-        item.value = "";
-      });
-      setregEmail('');
-      setregClinicName('');
-      setregPassword('');
-      setregPhoneNumber('');
-      setregAddress('');
-      setUserLoginEmail('');
-      setUserLoginPassword('');
+    if(mode === 'registerFormInput'){
+      setRegInputFieldValue({ email: '', password: '', clinicName: '', phoneNumber: '', address: ''});
+      setLoginFieldValue({userLoginEmail: '', userLoginPassword: ''})
     }
-    else if(mode === "consultationRecordInput"){
-      setNewDoctorName('');
-      setNewPatientName('');
-      setNewDiagnosis('');
-      setNewMedication('');
-      setNewConsultationFee('');
-      setNewDateTime('');
-      setNewFollowUp('');
+    else if(mode === 'consultationRecordInput'){
+      setNewConsultationRecord({ doctor_name: '',  patient_name: '', diagnosis: '', medication: '', consultation_fee: '', date_time: '', follow_up: ''});
     }
-    
-    
-
-    
   }
+
+  const handleOnChange = (e, mode)=>{
+    var temp = {};
+    if(mode==='register'){
+      for(var key in regInputFieldValue){
+        if(key == e.target.name){ temp[key] = e.target.value; }
+        else{ temp[key] = regInputFieldValue[key]; }
+      }
+      setRegInputFieldValue(temp);
+    }
+    else if(mode==="login"){
+      for(var key in loginFieldValue){
+        if(key == e.target.name){ temp[key] = e.target.value; }
+        else{ temp[key] = loginFieldValue[key]; }
+      }
+      setLoginFieldValue(temp);
+    }
+    else if(mode==="newConsultationRecord"){
+      for(var key in newConsultationRecord){
+        if(key == e.target.name){ temp[key] = e.target.value; }
+        else{ temp[key] = newConsultationRecord[key]; }
+      }
+      setNewConsultationRecord(temp);
+    
+    }
+  }
+
 
   if(isLoggedIn === false){
     if(pageState === "register"){
       return <div className="App">
-      <div className="form registration-form">
-      <h1>Clinic Registration</h1>
-        <div className="fieldName">Email: </div>
-        <input type="text" name="email" onChange={(e)=>{
-          setregEmail(e.target.value)
-        }}></input>
-        <div className="fieldName">Password: </div>
-        <input type="text" name="password" onChange={(e)=>{
-          setregPassword(e.target.value)
-        }}></input>
-        <div className="fieldName">Clinic Name: </div>
-        <input type="text" name="clinicName" onChange={(e)=>{
-          setregClinicName(e.target.value)
-        }}></input>
-        <div className="fieldName">Phone number: </div>
-        <input type="text" name="phoneNumber" onChange={(e)=>{
-          setregPhoneNumber(e.target.value)
-        }}></input>
-        <div className="fieldName">Address: </div>
-        <input type="text" name="address" onChange={(e)=>{
-          setregAddress(e.target.value)
-        }}></input>
-        <button onClick={registerButtonClicked}>Register</button>
+        <div className="form registration-form">
+          <h1>Clinic Registration</h1>
+          {Object.keys(regInputFieldValue).map((key)=>{
+            return [<div className="fieldName">{key}</div>,<input type="text" name={key} onChange={(e)=>{handleOnChange(e, 'register')}} value={regInputFieldValue[key]}></input>]
+          })}
+          <button onClick={registerButtonClicked}>Register</button>
         </div>
         <div className="go-login">Already have an account?</div>
         <button className="go-login" onClick={()=>{
           setPageState("login")
-          clearField("registerFormInput");
+          clearField();
         }}>Go login</button>
       </div>
-      }
-      if(pageState === "login"){
-       
-        
-        return<div className="App"><div className="form login-form">
-        <h1>Clinic User Log In</h1>
-          <input type="text" name="userLoginEmail" onChange={(e)=>{
-            setUserLoginEmail(e.target.value)
-          }} placeholder="Email..."></input>
-          <input type="text" name="userLoginPassword" onChange={(e)=>{
-            setUserLoginPassword(e.target.value)
-          }} placeholder="Password..."></input>
-          <button onClick={loginButtonClicked}>Login</button>
-          </div>
-          <div className="go-register">Don't have account yet?</div>
-          <button className="go-register" onClick={()=>{
-            setPageState("register")
-            clearField("registerFormInput");
-          }}>Go register</button>
-        </div>
-      }
-      else{
-        return <h1>Error before login</h1>
-      }
+    }
+    else if(pageState === "login"){
+      return<div className="App"><div className="form login-form">
+      <h1>Clinic User Log In</h1>
+      {Object.keys(loginFieldValue).map((key)=>{
+            return <input type="text" name={key} onChange={(e)=>{handleOnChange(e, 'login')}} placeholder={key.replace('userLogin','')+"..."} value={loginFieldValue[key]}></input>
+      })}
+      <button onClick={loginButtonClicked}>Login</button>
+      </div>
+      <div className="go-register">Don't have account yet?</div>
+      <button className="go-register" onClick={()=>{
+        setPageState("register")
+        clearField("registerFormInput");
+      }}>Go register</button>
+      </div>
+    }
+    else{
+      return <h1>Error before login</h1>
+    }
       
 
       
@@ -247,13 +230,9 @@ function App() {
         <h1>Welcome - {userList[0].clinicName}</h1>
         <button onClick={logoutButtonClicked} className="logout">Logout</button>
         <div className="consultation-record-container">
-            <div className="consultation-record-item title">Doctor Name</div>
-            <div className="consultation-record-item title">Patient Name</div>
-            <div className="consultation-record-item title">Diagnosis</div>
-            <div className="consultation-record-item title">Medication</div>
-            <div className="consultation-record-item title">Fee</div>
-            <div className="consultation-record-item title">Date Time</div>
-            <div className="consultation-record-item title">Follow Up?</div>
+            {Object.keys(newConsultationRecord).map((key)=>{
+                return <div type="text" className="consultation-record-item title">{key.replace('_',' ')}</div>
+            })}
             {recordList.map((row)=>{
               return Object.keys(row).map((element)=>{
                 return <div className="consultation-record-item">{row[element]}</div>
@@ -272,34 +251,11 @@ function App() {
       return <div className="form create-record-form">
         <h1>Clinc Name - {userList[0].clinicName}</h1>
         <button onClick={logoutButtonClicked} className="logout">Logout</button>
-        <div className="fieldName">Doctor Name: </div>
-        <input type="text" name="doctor_name" onChange={(e)=>{
-          setNewDoctorName(e.target.value)
-        }}></input>
-        <div className="fieldName">Paitent Name: </div>
-        <input type="text" name="patient_name" onChange={(e)=>{
-          setNewPatientName(e.target.value)
-        }}></input>
-        <div className="fieldName">Diagnosis: </div>
-        <input type="text" name="diagnosis" onChange={(e)=>{
-          setNewDiagnosis(e.target.value)
-        }}></input>
-        <div className="fieldName">Medication: </div>
-        <input type="text" name="medication" onChange={(e)=>{
-          setNewMedication(e.target.value)
-        }}></input>
-        <div className="fieldName">Consultation Fee: </div>
-        <input type="text" name="consultation_fee" onChange={(e)=>{
-          setNewConsultationFee(e.target.value)
-        }}></input>
-        <div className="fieldName">Date and Time: </div>
-        <input type="text" name="date_time" onChange={(e)=>{
-          setNewDateTime(e.target.value)
-        }}></input>
-        <div className="fieldName">Follow up consultation: </div>
-        <input type="text" name="follow_up" onChange={(e)=>{
-          setNewFollowUp(e.target.value)
-        }}></input>
+        
+        {Object.keys(newConsultationRecord).map((key)=>{
+            return [<div className="fieldName">{key.replace("_"," ")}</div>,<input type="text" name={key} onChange={(e)=>{handleOnChange(e, 'newConsultationRecord')}} value={newConsultationRecord[key]}></input>]
+        })}
+
         <button onClick={()=>{
             setPageState("login-browse-record")
           }
