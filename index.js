2b2b9d3085727acd9a12d388dec26c2227bc5c3f
DixonYeung <dixonycf@gmail.com>
2021-05-24 02:25:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const db = mysql.createPool({
     password: 'clinic_app',
     database: "clinic"
 });
-var whitelist = ["https://dizzon-webapp-todolist.herokuapp.com","https://dizzonwebapp-todolist-6wnm8.ondigitalocean.app","https://dizzonwebapp-todolist.on.fleek.co","https://dizzonwebapp-todolist.netlify.app"];
+var whitelist = ["https://dizzon-webapp-todolist.herokuapp.com","https://dizzonwebapp-todolist-6wnm8.ondigitalocean.app","https://dizzonwebapp-todolist.on.fleek.co","https://dizzon-todolist.netlify.app"];
 app.use(cors({
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {
