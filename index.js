41d86fba8b0a157db32c3d35ddfe8e9552868b56
DixonYeung <dixonycf@gmail.com>
2021-05-24 02:19:59 +0800

    added netlify domain to CORS

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const db = mysql.createPool({
     password: 'clinic_app',
     database: "clinic"
 });
-var whitelist = ["https://dizzon-webapp-todolist.herokuapp.com","https://dizzonwebapp-todolist-6wnm8.ondigitalocean.app","https://dizzonwebapp-todolist.on.fleek.co"];
+var whitelist = ["https://dizzon-webapp-todolist.herokuapp.com","https://dizzonwebapp-todolist-6wnm8.ondigitalocean.app","https://dizzonwebapp-todolist.on.fleek.co","https://dizzonwebapp-todolist.netlify.app"];
 app.use(cors({
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {
