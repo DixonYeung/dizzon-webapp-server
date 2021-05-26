4ec21b3ca7545ee5c38998841ea1b997d125416a
DixonYeung <dixonycf@gmail.com>
2021-05-27 00:45:00 +0800

    upload heroku repo to github repo

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const saltRounds = 10;
 const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
 
+const {MongoClient} = require('mongodb');
+
 const PORT = process.env.PORT || 5000;
 
 
@@ -25,6 +27,8 @@ const db = mysql.createPool({
     password: 'clinic_app',
     database: "clinic"
 });
+
+
 var whitelist = ["https://dizzon-webapp-todolist.herokuapp.com","https://dizzonwebapp-todolist-6wnm8.ondigitalocean.app","https://dizzonwebapp-todolist.on.fleek.co","https://dizzon-todolist.netlify.app"];
 app.use(cors({
     origin: function (origin, callback) {
@@ -52,12 +56,185 @@ app.use(expressSession({
 }))
 
 
+
+//db
+async function main(mode){
+    const uri = process.env.MONGO_URI;
+    const mongodb =  new MongoClient(uri,{ useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+        // Connect to the MongoDB cluster
+        await mongodb.connect();
+        
+        // Make the appropriate DB calls
+        if(mode == "listdb"){
+            return await listDatabases(mongodb);
+        }
+        else if(mode == "createCollection"){
+            return await createCollection(mongodb);
+        }
+        else if(mode == "listAllMarker"){
+            return await listAllMarker(mongodb);
+        }
+        else if(mode == "insertAllMarker"){
+            return await insertAllMarker(mongodb);
+        }
+        else if(mode =="listCollection"){
+            return await listCollection(mongodb);
+        }
+        else if(mode =="deleteAllMarker"){
+            return await deleteAllMarker(mongodb);
+        }
+
+    } catch (e) {
+        console.error(e);
+    } 
+    // finally {
+        //await mongodb.close();
+    //}
+}
+async function listDatabases(client){
+    const databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    let temp = "";
+    databasesList.databases.forEach(db => temp = temp + db.name + "<br>");
+    console.log(temp);
+    client.close();
+    return temp;
+};
+
+async function createCollection(client){
+    
+    await client.db().createCollection("marker_info", function(err, res) {
+        if (err) {
+            // console.log(err);
+            // client.close();
+            return "error occurred";
+        }
+        console.log("Collection created!");
+        client.close();
+        return "create collection success";
+    });
+    
+};
+
+
+async function listCollection(client){
+    return new Promise(function(resolve, reject){
+        client.db().listCollections().toArray( function(err, collectionList){
+            if(err){
+                console.log(err);
+                resolve("error in marker_info find");
+            }
+            console.log(JSON.stringify(collectionList));
+            client.close();
+            resolve(JSON.stringify(collectionList));
+        });
+    }); 
+};
+
+
+async function listAllMarker(client){
+    return new Promise(function(resolve, reject){
+        client.db().collection("marker_info").find({}).toArray( function(err, result){
+            if(err){
+                console.log(err);
+                resolve( "error in marker_info find" );
+            }
+            console.log(JSON.stringify(result));
+            client.close();
+            resolve(JSON.stringify(result));
+        });
+    }); 
+};
+
+async function deleteAllMarker(client){
+    return new Promise(function(resolve, reject){
+        client.db().collection("marker_info").find({}).toArray( function(err, result){
+            if(err){
+                console.log(err);
+                resolve( "error in marker_info find" );
+            }
+            console.log("delete all marker: find all marker successfully");
+            resolve(result);
+        });
+    }).then(function(result){
+        if(result.length > 0){
+            return new Promise(function(resolve, reject){
+                client.db().collection("marker_info").deleteMany( {} ,function(err, result){
+                    if(err){
+                        console.log(err);
+                        resolve( "error in marker_info delete all" );
+                    }
+                    console.log("all marker deleted");
+                    client.close();
+                    resolve("all marker deleted");
+                });
+            })
+        }
+    });
+    
+};
+
+async function insertAllMarker(client){
+    return new Promise(function(resolve, reject){
+        client.db().collection("marker_info").find({}).toArray( function(err, result){
+            if(err){
+                console.log(err);
+                resolve( "error in marker_info find" );
+            }
+            console.log("insert all marker: find all marker successfully");
+            resolve(result);
+        });
+    }).then(function(result){
+        if( ! (result.length > 0) ){
+            return new Promise(function(resolve, reject){
+                let data = require("./marker_info.json");
+                client.db().collection("marker_info").insertMany( data ,function(err, result){
+                    if(err){
+                        console.log(err);
+                        resolve( "error in marker_info insert all" );
+                    }
+                    console.log("all marker inserted");
+                    client.close();
+                    resolve("all marker inserted");
+                });
+            })
+        }
+    });
+    
+};
+
+
+
+//db api
+app.get('/api/listdb', async (req, res) => {
+    res.send( await main('listdb').catch(console.error) );
+});
+app.get('/api/listAllMarker', async (req, res) => {
+    res.send( await main('listAllMarker').catch(console.error) );
+});
+app.get('/api/createCollection', async (req, res) => {
+    res.send( await main('createCollection').catch(console.error) );
+});
+app.get('/api/listCollection', async (req, res) => {
+    res.send( await main('listCollection').catch(console.error) );
+});
+app.get('/api/insertAllMarker', async (req, res) => {
+    res.send( await main('insertAllMarker').catch(console.error) );
+});
+app.get('/api/deleteAllMarker', async (req, res) => {
+    res.send( await main('deleteAllMarker').catch(console.error) );
+});
+
+
+//to do list api
 app.get('/api/todolist', (req, res) => {
     let data = require('./todolist.json');
     res.send(data);
 });
 
-
+//rest of the api
 app.get('/api/get', (req, res) => {
     const sqlSelect = "SELECT email, clinicName, phoneNumber, address FROM userInfo;";
     db.query(sqlSelect, (err, result) =>{
